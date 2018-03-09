12ffa123086eb1394a7167106d75a0870d4d3fa1
BIT Student <classroom@bgit.rs>
2018-03-09 17:56:05 +0100

    Show must go on

--- jQuery/Traverse(redo gallery)/main.js
+++ jQuery/Traverse(redo gallery)/main.js
@@ -1,5 +1,5 @@
 
-$('.selected').removeClass( "selected" ).parent().siblings().children().next().next().prev().addClass('selected');
+$('.selected').removeClass( "selected" ).parent().siblings().children().next().next().prev().addClass('selected').fadeOut(3000).animate({bottom: '-150px'}, 2).fadeIn(3000);
 
-//.fadeOut(3000).animate({bottom: '-150px'}, 2).fadeIn(3000)
+
 
