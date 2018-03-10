dcbde82d40eb3c886dececcf476519b683358a56
BIT student <classroom@bgit.rs>
2018-03-11 00:58:08 +0100

    BitShow continue

--- BitShow/js/dataModule.js
+++ BitShow/js/dataModule.js
@@ -1,33 +1,33 @@
 const dataModule = (function () {
     class Show {
-        constructor(name, image, id, seasons, casts, details) {
+        constructor(name, image, id, summary) {
             this.name = name;
             this.image = image;
             this.id = id;
             this.seasons = [];
             this.casts = [];
-            this.details = details;
+            this.summary = summary;
         }
     }
 
     class TVShow {
-        constructor(name, url, id) {
-            this.name = name;
-            this.url = url;
-            this.id = id;
+        constructor(showArr) {
+            this.showArr = showArr;
         }
     }
 
-    const createTVShow = (name, url, id) => {
-        return new TVShow(name, url, id);
+    const createShow = (name, image, id, summary) => {
+        return new Show(name, image, id, summary);
     }
 
     const adaptShowList = (showList) => {
         return showList.map((show) => {
-            return createTVShow(show.name, show.image.medium, show.id);
+            return createShow(show.name, show.image.medium, show.id, show.summary);
         })
     }
-
+    const tvShowClass = (showArr) => {
+        return new TVShow(showArr);
+    }
     class Season {
         constructor(startDate, endDate) {
             this.startDate = new Date(startDate);
@@ -43,7 +43,8 @@ const dataModule = (function () {
     }
 
     return {
-        createTVShow,
-        adaptShowList
+        createShow,
+        adaptShowList,
+        tvShowClass
     }
 })();
